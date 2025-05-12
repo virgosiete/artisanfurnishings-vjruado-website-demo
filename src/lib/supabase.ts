aaf51fa256c82e56c408e141efe0b090d47db822
OGRIV <30544756+virgosiete@users.noreply.github.com>
2025-05-12 12:32:33 +0800

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These environment variables are automatically provided when you connect your project to Supabase
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL=https://xyzcompany.supabase.co;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL=https://bqfuphqpimtrfspqnhbo.supabase.co;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZnVwaHFwaW10cmZzcHFuaGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMTk1NzQsImV4cCI6MjA2MjU5NTU3NH0.luUudV5JQmF428-GsJovluBzgkyI_QcaUwBeCUWxf5c;
 
 if (!supabaseUrl || !supabaseAnonKey) {
